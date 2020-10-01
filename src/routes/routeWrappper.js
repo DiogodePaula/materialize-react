a5bba953ec74f1b41b8825e88beba92ca101735c
DiogodePaula <depaulatrabalhos@gmail.com>
2020-09-30 23:49:09 -0300

    iniciando o styled components

---
+++ src/routes/routeWrappper.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { Route, Redirect } from 'react-router-dom';
+
+export default function RoutWrapper({
+  component: Component,
+  isPrivate,
+  ...rest
+}) {
+  const user = useSelector((state) => state.login);
+  let signed = false;
+
+  if (user) {
+    signed = true;
+  }
+
+  // rota caso user n esteja logado
+  if (!signed && isPrivate) {
+    return <Redirect to="/login" />;
+  }
+
+  if (signed && !isPrivate) {
+    return <Redirect to="/" />;
+  }
+
+  return (
+    <Route
+      {...rest}
+      render={(props) => (
+        <>
+          <Component {...props} />
+        </>
+      )}
+    />
+  );
+}
